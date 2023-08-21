afb01ef0983323fb637e839c982b119f950bd8bf
Samintha Chandrasiri <saminthachandrasiri@Saminthas-MacBook-Pro.local>
2023-08-21 14:01:51 -0700

    firstcommit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+
+import Post from '../mongodb/models/post.js';
+
+
+dotenv.config();
+
+
+const router = express.Router();
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_APT_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+router.route('/').get(async(req, res) => {
+    try{
+        const posts = await Post.find({});
+        res.status(200).json({success: true, data: posts })
+
+    } catch (err) {
+        res.status(500).json({success: false, message: error })
+    }
+
+});
+
+router.route('/').post(async(req, res) => {
+    try{ 
+        console.log('reaches post func')
+        const { name, prompt, photo } = req.body;
+        console.log(name, prompt, photo.slice(22, 100))
+        const photoUrl = await cloudinary.uploader.upload(photo);
+        console.log('cloudinary')
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        })
+        res.status(200).json({ success: true, data: newPost });
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+});
+
+export default router;
